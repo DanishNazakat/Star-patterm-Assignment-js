b25d25ee6625aa2eded74920f56a5d077f4e9452
DanishNazakat <danishnazakat636@gmail.com>
2025-06-02 20:36:03 -0700

    js Assignment

---
+++ app.js
@@ -0,0 +1,45 @@
+
+let num = 0;
+
+for(let i = 1; i <= 4; i++){
+    // console.log(num);
+    for(let j = 1; j <= i ; j++){
+        // document.write(i + "<br />");
+     
+        document.writeln(i)
+    }
+          document.writeln( "<br />");
+}
+
+
+for(let i = 1 ; i <= 4 ; i++){
+    for(let j = 1; j <= i ; j++){
+       document.write(j);
+       
+    }
+    document.write( "<br />")
+}
+
+
+
+// Number Increasing Reverse Pyramid
+document.write("dasljlasjksdlj")
+for(let i = 4; i >= 1; i--){
+    document.write( "<br />");
+    for(let j = 1; j <= i;j++ ){
+        document.write(j)
+    }
+}
+
+
+
+
+// Number changing Pyramid
+
+for(let i = 1; i <= 4; i++){
+    
+    console.log(i);
+    for(let j = 1; j <= i ; j++){
+        document.write(j);
+    }
+}
